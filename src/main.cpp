a6c5da81394eda47b54a994dac4a01b66cdf7081
Spencer Chen <Wen.ShanSpencer.Chen@power.com>
2023-07-05 14:20:16 +0800

    Test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,33 @@
 #include <Arduino.h>
+#include <Wire.h>
+//Step  1  :  Add the Header Files
 #include <Drv_Rtc.h>
 #include <Drv_i2c.h>
 #include <Inno3Pro.h>
 #include <Config.h>
-// put function declarations here:
-int myFunction(int, int);
+#define NO_RESPONSE 0x00
+#define AUTO_RESTART 0x02
+//Step  2  :  Create the class instance
+Inno3Pro_Application Inno3ProApp;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//Step 3 :  Write Initial Commands to Inno Pro
+void setup() 
+{ 
+  Wire.begin();
+	Inno3ProApp.Inno3Pro_Initialization(); 
+  Inno3ProApp.Inno3Pro_Write_CCSC_Fault_Response(NO_RESPONSE);
+  Inno3ProApp.Inno3Pro_Write_CVOL_Fault_Timer(0x03);
+  // 5V, 5.1A , Voltage and Constant Current
+  Inno3ProApp.Inno3Pro_Write_VI(24 , 3.4);   
+  // ON       , Vbus Enable
+  Inno3ProApp.Inno3Pro_Vbus_Switch_Control(1);     
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+//Step 4 :  Call the Functions on the Main Loop 
+void loop()
+{
+      //Control Functions Set-Up
+      
+                  
 }
